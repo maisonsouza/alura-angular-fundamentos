bf0625db9473a64ca23f365a61f18277d9d8ed51
Maison <maisongalvao144@gmail.com>
2019-12-07 18:09:49 -0400

    Aula01 - Bem começado metade feito.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'ifamdrangular';
+  description = "ifam";
+  url = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Instituto_Federal_do_Amazonas_-_Marca_Vertical_2015.svg/1200px-Instituto_Federal_do_Amazonas_-_Marca_Vertical_2015.svg.png";
 }
